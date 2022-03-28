16eb098d8e0631133237bbca5716a1beb3b80529
Felix Steinle <steinle@westphalia-datalab.com>
2022-03-28 14:11:02 +0200

    init env

--- src/fin_app_backend/hooks.py
+++ src/fin_app_backend/hooks.py
@@ -6,14 +6,25 @@ from kedro.framework.hooks import hook_impl
 from kedro.io import DataCatalog
 from kedro.versioning import Journal
 
+import os
+
+from kedro.config import TemplatedConfigLoader
 
 class ProjectHooks:
     @hook_impl
-    def register_config_loader(
-        self, conf_paths: Iterable[str], env: str, extra_params: Dict[str, Any],
-    ) -> ConfigLoader:
-        return ConfigLoader(conf_paths)
-
+    def register_config_loader(self, conf_paths: Iterable[str]) -> TemplatedConfigLoader:
+        return TemplatedConfigLoader(
+            conf_paths,
+            globals_dict={
+                "POSTGRES_USER": os.environ.get("POSTGRES_USER"),
+                "POSTGRES_PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+                "POSTGRES_HOST": os.environ.get("POSTGRES_HOST"),
+                "POSTGRES_PORT": os.environ.get("POSTGRES_PORT"),
+                "POSTGRES_DB": os.environ.get("POSTGRES_DB"),
+                "FINANCE_API": os.environ.get("FINANCE_API"),
+                "SCHEME_DB": os.environ.get("SCHEME_DB"),
+            },
+        )
     @hook_impl
     def register_catalog(
         self,
